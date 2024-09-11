1b5a54cf51c491219648a7a392c9f38985253bc6
hyunwoo <hyunwoo20051120@gmail.com>
2024-09-11 17:36:36 +0900

    first commit

--- src/DataTypesExample.java
+++ src/DataTypesExample.java
@@ -11,6 +11,5 @@ public class DataTypesExample {
         System.out.println("x: " + x); // x는 10으로 유지
         System.out.println("y: " + y); // y는 20으로 변경
         System.out.println("arr1[0]: " + arr1[0]); // arr1[0]도 100으로 변경 (arr1과 arr2는 같은 배열을 참조)
-        System.out.println("arr2[1]: " + arr2[1]);
     }
 }
--- src/QuestionWeek2.java
+++
@@ -1,9 +0,0 @@
-public class QuestionWeek2 {
-    public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3}; // 참조형
-        int[] arr2 = arr1; // 주소 복사
-        arr2[0] = 100; // arr2를 통한 값 변경
-        System.out.println(approximatePi);
-
-    }
-}
